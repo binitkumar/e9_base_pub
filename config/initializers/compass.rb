1bdaf74b88d02410d1ef624f5881be384eec8e13
Travis <numbers1311407@gmail.com>
2012-02-27 15:07:46 -0500

    made e9_base behave like a compass plugin, removed empty bogus scss template

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,13 +1,9 @@
 # fall back to the gem's auth rules if their config is not found locally
-if defined?(Rails) && File.exists?(config = File.join(Rails.root, 'config', 'compass.rb'))
+if defined?(Rails) && Rails.env.development? && File.exists?(config = File.join(Rails.root, 'config', 'compass.rb'))
   require 'sass'
   require 'compass'
   require 'rgbapng'
-
-  Compass::Frameworks.register("e9_base",
-    :stylesheets_directory  => File.join(File.dirname(__FILE__), "../../app/stylesheets"),
-    :templates_directory    => File.join(File.dirname(__FILE__), "../../app/templates")
-  )
+  require 'e9/compass'
 
   Compass.add_project_configuration(config)
   Compass.configure_sass_plugin!
---
+++ lib/e9/compass.rb
@@ -0,0 +1,4 @@
+Compass::Frameworks.register("e9_base",
+  :stylesheets_directory  => File.join(File.dirname(__FILE__), "../../app/stylesheets"),
+  :templates_directory    => File.join(File.dirname(__FILE__), "../../app/templates")
+)
