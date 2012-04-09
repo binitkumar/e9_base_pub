25c7f6183628a365b3b4798ae24bcdc6c5e87277
Travis <numbers1311407@gmail.com>
2012-04-09 15:04:30 -0400

    insert staticcache after rack:cache

--- config/initializers/static_cache.rb
+++ config/initializers/static_cache.rb
@@ -1,7 +1,8 @@
+require 'rack'
 require 'rack/contrib'
 
-Rails.application.config.middleware.use Rack::StaticCache, {
+Rails.application.config.middleware.insert_after('Rack::Cache', Rack::StaticCache, {
   :urls       => E9Base.static_paths,
   :root       => E9Base::ASSETS_DIR,
   :versioning => false
-}
+})
